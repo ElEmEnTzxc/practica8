799e63347049c3b931ffe74fb34421f46e1f5ab2
root <root@ip-172-31-88-123.ec2.internal>
2019-12-16 22:58:27 +0000

    ole

--- front2.sh
+++ front2.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-set -x
-
 #Actualizamos los repositorios
 apt-get update
 
@@ -17,9 +15,6 @@ cd adminer
 wget https://github.com/vrana/adminer/releases/download/v4.3.1/adminer-4.3.1-mysql.php
 mv adminer-4.3.1-mysql.php index.php
 
-#Instalamos git
-apt-get install git -y
-
 # Instalación de php-fpm y php-mysql
 apt-get install php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip -y
 apt-get install php-fpm php-mysql -y
@@ -28,24 +23,13 @@ apt-get install php-fpm php-mysql -y
 cd /etc/php/7.2/fpm/
 sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/' php.ini
 
-
-
-
-# Concedemos permisos a Wordpress
-chown www-data:www-data * -R
-
 # Instalamos el cliente NFS
-sudo apt-get install nfs-common -y
+apt-get install nfs-common -y
 
 # Creamos el punto de montaje en el cliente NFS
-mkdir wordpress
-sudo mount 3.82.136.54:/var/www/html/wordpress/ /var/www/html/wordpress/
-
-
-
-
-
-
+cd /var/www/html/
+mkdir wordpress/
+mount 54.196.8.122:/var/www/html/wordpress /var/www/html/wordpress
 
 
 
