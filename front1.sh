5f9ce25f3c5e27bf1497d801cf70b3ffc5d74bcb
rafa rsm <43401512+ElEmEnTzxc@users.noreply.github.com>
2019-12-17 13:57:57 +0100

    Update front1.sh

--- front1.sh
+++ front1.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-set -x
-
 #Actualizamos los repositorios
 apt-get update
 
@@ -13,7 +11,7 @@ apt-get install php libapache2-mod-php php-mysql -y
 #Instalamos adminer
 cd /var/www/html
 mkdir adminer
-cd adminerd
+cd adminer
 wget https://github.com/vrana/adminer/releases/download/v4.3.1/adminer-4.3.1-mysql.php
 mv adminer-4.3.1-mysql.php index.php
 
@@ -30,61 +28,67 @@ cd /var/www/html
 wget https://wordpress.org/latest.tar.gz
 
 # Descomprimimos el archivo recién descargado
-tar -zxvf latest.tar.gz
+tar -zxvf latest.tar.gz 
+cp /var/www/html/wordpress/* /var/www/html/
+rm -r wordpress
 
 # Modificamos el archivo wp-config-example.php
-cd /var/www/html/wordpress
-mv wp-config-sample.php wp-config.php
-sed -i 's/database_name_here/wordpress/' wp-config.php
-sed -i 's/username_here/wordpress/' wp-config.php
-sed -i 's/password_here/wordpress/' wp-config.php
-sed -i 's/localhost/54.208.146.205/' wp-config.php
+
+mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+
+sed -i 's/database_name_here/wordpress' /var/www/html/wp-config.php
+sed -i 's/username_here/wordpress' /var/www/html/wp-config.php
+sed -i 's/password_here/wordpress' /var/www/html/wp-config.php
+sed -i 's/localhost/3.94.203.50/' /var/www/html/wp-config.php
 
 # Concedemos permisos a Wordpress
 chown www-data:www-data * -R
 
 # Instalamos el servidor NFS
-sudo apt-get install nfs-kernel-server -y
-
-# Cambiamos los permisos al directorio que vamos a compartir
-sudo chown nobody:nogroup /var/www/html/wordpress/
-
-# Editamos el archivo /etc/exports
-cd /etc/
-echo "/var/www/html/wordpress/     35.173.133.252(rw,sync,no_root_squash,no_subtree_check)" > /etc/exports
+apt-get install nfs-kernel-server -y
 
 
-# Reiniciamos el servicio nfs-kernel-server
-sudo /etc/init.d/nfs-kernel-server restart
 
-# Dirección del sitio y direccion URL
-cd /var/www/html/wordpress/
-echo "define( 'WP_SITEURL', 'http://52.23.233.247/wordpress' );" >> wp-config.php
-echo "define( 'WP_HOME', 'http://52.23.233.247' );" >> wp-config.php
+# Dirección del sitio y direccion URL 
+cd /var/www/html/
+echo "define( 'WP_SITEURL', 'http://34.205.25.119' );" >> wp-config.php
+echo "define( 'WP_HOME', 'http://34.205.25.119' );" >> wp-config.php
 
 
-# Creamos un archivo .htaccess
-cd /home/ubuntu
-rm -r practica8
-sudo git clone https://github.com/ElEmEnTzxc/practica8.git
-mv /home/ubuntu/practica8/.htaccess /var/www/html/.htaccess
+
 
 #Creamos uploads
-mkdir /var/www/html/wordpress/wp-content/uploads -p
+mkdir /var/www/html/wp-content/uploads -p
 
 # Security Keys
 #Borramos las keys
-sed -i '/AUTH_KEY/d' /var/www/html/wordpress/wp-config.php
-sed -i '/LOGGED_IN_KEY/d' /var/www/html/wordpress/wp-config.php
-sed -i '/NONCE_KEY/d' /var/www/html/wordpress/wp-config.php
-sed -i '/AUTH_SALT/d' /var/www/html/wordpress/wp-config.php
-sed -i '/SECURE_AUTH_SALT/d' /var/www/html/wordpress/wp-config.php
-sed -i '/LOGGED_IN_SALT/d' /var/www/html/wordpress/wp-config.php
-sed -i '/NONCE_SALT/d' /var/www/html/wordpress/wp-config.php
+sed -i '/AUTH_KEY/d' /var/www/html/wp-config.php
+sed -i '/LOGGED_IN_KEY/d' /var/www/html/wp-config.php
+sed -i '/NONCE_KEY/d' /var/www/html/wp-config.php
+sed -i '/AUTH_SALT/d' /var/www/html/wp-config.php
+sed -i '/SECURE_AUTH_SALT/d' /var/www/html/wp-config.php
+sed -i '/LOGGED_IN_SALT/d' /var/www/html/wp-config.php
+sed -i '/NONCE_SALT/d' /var/www/html/wp-config.php
 
 #Añadimos las nuevas keys
 CLAVES=$(curl https://api.wordpress.org/secret-key/1.1/salt/)
 CLAVES=$(echo $CLAVES | tr / _)
-sed -i "/#@-/a $CLAVES" /var/www/html/wordpress/wp-config.php
+sed -i "/#@-/a $CLAVES" /var/www/html/wp-config.php
+
+
+#Borramos el index.html para que se nos rediriga al index.php
+cd /var/www/html
+rm -r index.html
+
+# Cambiamos los permisos al directorio que vamos a compartir
+chown nobody:nogroup /var/www/html/
+
+# Editamos el archivo /etc/exports
+cd /etc/
+echo "/var/www/html/      34.228.185.147(rw,sync,no_root_squash,no_subtree_check)" > /etc/exports
+
+
+# Reiniciamos el servicio nfs-kernel-server
+sudo /etc/init.d/nfs-kernel-server restart
 
 
