3e80767ca111dcc5b5a261eeec26243a754703cb
rafa rsm <43401512+ElEmEnTzxc@users.noreply.github.com>
2020-01-15 20:18:53 +0100

    Update wpbalance.sh

--- wpbalance.sh
+++ wpbalance.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#actualizamos los repositorios
+# Actualizamos los repositorios
 apt-get update
 
-#Instalamos apache
+# Instalamos apache2
 apt-get install apache2 -y
 
-#Instalamos paquetes para apache
+# Instalamos paquetes para apache
 apt-get install php libapache2-mod-php php-mysql -y
 
-#Activamos modulos
+# Activamos modulos del apache2
 a2enmod proxy
 a2enmod proxy_http
 a2enmod proxy_ajp
@@ -20,11 +20,11 @@ a2enmod proxy_connect
 a2enmod proxy_html
 a2enmod lbmethod_byrequests
 
-#Copiamos el archivo 000-default.conf que está a el que esta en el directorio /etc/apache2/sites-enabled
+# Copiamos el archivo 000-default.conf  en el directorio /etc/apache2/sites-enabled
 cd ~
 git clone https://github.com/ElEmEnTzxc/practica8
 cp practica8/000-default.conf /etc/apache2/sites-enabled/
 
-#Reiniciamos Apache
+# Reiniciamos Apache
 sudo /etc/init.d/apache2 restart
 
